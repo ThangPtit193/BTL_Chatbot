55543e4da29f0cc24ea685635f751fea4a965395
phongnt <phongnt@ftech.ai>
2022-09-06 15:51:51 +0700

    add basic config for init swagger

---
+++ core/config.py
@@ -0,0 +1,31 @@
+import os
+import pathlib
+
+from pydantic import AnyHttpUrl, BaseSettings, validator
+from typing import List, Union
+
+# Project Directories
+ROOT = pathlib.Path(__file__).resolve().parent.parent
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
