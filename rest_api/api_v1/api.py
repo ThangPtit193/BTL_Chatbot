0dbd09bff9a6806b7bd06a3613eab60851a2661a
phongnt <phongnt@ftech.ai>
2022-10-18 13:54:10 +0700

    update search endpoint

--- rest_api/api_v1/api.py
+++ rest_api/api_v1/api.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from rest_api.api_v1.endpoints.document_store import router as upload_router
+from rest_api.api_v1.endpoints.search import router as retrieve_router
 
 from rest_api.auth.router import router as authenticator_router
 
 api_router = APIRouter()
 api_router.include_router(upload_router, prefix="/document_store", tags=["document_store"])
 api_router.include_router(authenticator_router, prefix='/auth', tags=['auth'])
+api_router.include_router(retrieve_router, prefix="/retrieve", tags=["retrieve"])
