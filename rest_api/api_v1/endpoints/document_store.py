f7bdd1a7f29c5fe08865a1cf82e991177213b2cc
phongnt <phongnt@ftech.ai>
2022-10-18 13:53:42 +0700

    update document store

--- rest_api/api_v1/endpoints/document_store.py
+++ rest_api/api_v1/endpoints/document_store.py
@@ -1,43 +1,44 @@
 import os
 import shutil
 import uuid
+from enum import Enum
 from pathlib import Path
-from typing import List, ClassVar
+from typing import List
+import ast
 
-from fastapi import APIRouter, UploadFile, File, Depends, Query
+from fastapi import APIRouter, UploadFile, File, Depends
 from fastapi.security import HTTPBearer
-from fastapi import HTTPException, status
 
 from test.sample_pipelines import VenusServices
+from rest_api.config import PROJECT_DIR, DocumentStore
 
 router = APIRouter()
-# upload_service.get_config_params("/Users/phongnt/FTECH/knowledge-retrieval/rest_api/pipelines/master-pipelines.yml")
 
 reusable_oauth2 = HTTPBearer(
     scheme_name='Authorization'
 )
 
 
+# class ModelRetriever(str, Enum):
+#     va_base_retriever = "va-base-distilbert-multilingual-faq-v0.1.0"
+
+
 @router.post("/upload_file", dependencies=[Depends(reusable_oauth2)])
 async def upload_file(
-        # pretrained_retriever_model: str = Query("fschool-distilbert-multilingual-faq-v2.0.0",
-        #                                         enum=("fschool-distilbert-multilingual-faq-v2.0.0")),
-        retriever_selector: str = Query("fschool-distilbert-multilingual-faq-v2.0.0",
-                                        enum=("fschool-distilbert-multilingual-faq-v2.0.0",
-                                              "distilbert-multilingual-faq-v3.2")),
+        document_store_type: DocumentStore,
         files: List[UploadFile] = File(...)
 ):
     """
     This endpoint allows you upload multiple documents to document store
     """
-    venus_services = VenusServices(retriever_pretrained=retriever_selector, host="localhost", port=9200)
-    parent_file_path = Path("rest_api/upload")
+
+    parent_file_path = Path(os.path.join(PROJECT_DIR, "rest_api/upload"))
     file_paths = []
     # clear cache
-    if os.path.exists(os.path.abspath(parent_file_path)):
-        list_dir = os.listdir(os.path.abspath(parent_file_path))
+    if os.path.exists(parent_file_path):
+        list_dir = os.listdir(parent_file_path)
         if len(list_dir) > 10:
-            shutil.rmtree(os.path.abspath(parent_file_path))
+            shutil.rmtree(parent_file_path)
 
     if not os.path.exists(parent_file_path):
         os.mkdir(parent_file_path)
@@ -48,17 +49,54 @@ async def upload_file(
             with file_path.open("wb") as buffer:
                 shutil.copyfileobj(file.file, buffer)
 
-            file_paths.append(os.path.abspath(file_path))
+            file_paths.append(file_path)
         finally:
             file.file.close()
 
-    return venus_services.run(file_paths=file_paths)
+    return VenusServices.init_instance(document_store_type=document_store_type).run(file_paths=file_paths)
 
 
 @router.get("/get_indices")
-async def get_indices(document_store_type: str = Query("elasticsearch", enum=("elasticsearch", "inmemory"))):
+async def get_indices(document_store_type: DocumentStore):
     """
     This endpoint allows you get all indices stored in document store
     """
-    venus_services = VenusServices(document_store_type=document_store_type, host="localhost", port=9200)
-    return venus_services.get_all_indices()
+    # if document_store_type == "inmemory":
+    #     return {"message": "Querying index does not support for InMemoryDocumentStore"}
+    return VenusServices.init_instance(document_store_type=document_store_type).get_all_indices()
+
+
+@router.delete("/delete_documents/{index}", dependencies=[Depends(reusable_oauth2)])
+async def delete_documents_by_index(
+        document_store_type: DocumentStore,
+        index: str
+):
+    venus_services = VenusServices.init_instance(document_store_type=document_store_type)
+    indices = venus_services.get_all_indices()
+    if len(indices) == 0:
+        return {"message": "No index found to delete"}
+
+    return venus_services.delete_all_documents(index=index)
+
+
+@router.get("/get_all_documents_by_index/{index}")
+async def get_all_documents_by_index(document_store_type: DocumentStore, index: str):
+    try:
+        _documents = VenusServices.init_instance(document_store_type=document_store_type).get_all_documents_by_index(
+            index=index)
+        return ast.literal_eval(str(_documents))
+    except:
+        return {"message": f"{index} not found"}
+
+
+@router.delete("/delete_index/{index}")
+async def delete_index(document_store_type: DocumentStore, index: str):
+    if document_store_type == "inmemory":
+        return {"message": "Deleting index does not support for InMemoryDocumentStore"}
+    return VenusServices.init_instance(document_store_type=document_store_type).delete_index(index=index,
+                                                                                             document_store=document_store_type)
+
+
+@router.get("/get_all_documents")
+async def get_all_documents(document_store_type: DocumentStore):
+    return VenusServices.init_instance(document_store_type=document_store_type).get_all_documents()
