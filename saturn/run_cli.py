1acb26a77a398fff199ebd74bc93911a68ef5910
haonv <haonv@ftech.ai>
2022-12-16 13:12:35 +0700

    update cli

--- saturn/run_cli.py
+++ saturn/run_cli.py
@@ -3,7 +3,7 @@ import os
 import click
 from saturn.kr_manager import KRManager
 from comet.lib import logger
-
+from saturn.cli.model import model
 from .version import get_saturn_version
 
 logger.configure_logger("DEBUG")
@@ -49,47 +49,6 @@ def test(config, rtype, top_k, save_md):
 
 
 @click.command()
-@click.option('--model_path', '-p',
-              required=True,
-              help="path to folder",
-              default=None)
-@click.option('--name', '-n',
-              required=True,
-              help="The name of the model",
-              default=None)
-@click.option('--replace', '-rf',
-              help="Replace existed data/model",
-              is_flag=True)
-def push(model_path, name, replace):
-    from venus.wrapper import axiom_wrapper
-    axiom_wrapper.upload_model(model_name=name, dir_path=model_path, replace=replace)
-
-
-@click.command()
-def ls():
-    from venus.wrapper import axiom_wrapper
-    axiom_wrapper.ls_model()
-
-
-@click.command()
-@click.option('--name', '-n',
-              required=True,
-              help="The name of the model to pull",
-              default=None)
-@click.option('--output_path', '-dp',
-              required=True,
-              help="Where to save the model",
-              default="models")
-@click.option('--replace', '-rf',
-              help="Replace existed data/model",
-              is_flag=True
-              )
-def pull(name, dir_path, replace):
-    from venus.wrapper import axiom_wrapper
-    axiom_wrapper.download_model(model_name=name, dir_path=dir_path, replace=replace)
-
-
-@click.command()
 @click.option('--path', '-p',
               required=False,
               help="Path to run streamlit",
@@ -103,9 +62,6 @@ def ui(path):
 entry_point.add_command(version)
 entry_point.add_command(train)
 entry_point.add_command(test)
-entry_point.add_command(push)
-entry_point.add_command(pull)
-entry_point.add_command(ls)
 entry_point.add_command(ui)
 
 if __name__ == '__main__':
