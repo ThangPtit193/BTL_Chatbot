6fd64d07e461aa67c96c17ebe31a8b07d8639db6
Phương Nam <phuongnamDPN2k2@gmail.com>
2023-10-13 10:21:10 +0700

    Update train_biencoder.sh

--- scripts/train_biencoder.sh
+++ scripts/train_biencoder.sh
@@ -5,7 +5,7 @@ lr="5e-5"
 CUDA_VISIBLE_DEVICES=1 python3 saturn/train_biencoder.py \
         --model_dir $model_dir \
         --data_dir $data_dir\
-        --model_type unsim-cse-vietnamese \
+        --model_type GPT-HUST \
         --do_train \
         --num_train_epochs 10 \
         --train_batch_size 1024 \
