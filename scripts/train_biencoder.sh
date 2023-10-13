0aafe472edf9c94ed3ceb83d891ebddfc98d2088
Phương Nam <phuongnamDPN2k2@gmail.com>
2023-10-13 10:25:34 +0700

    Update train_biencoder.sh

--- scripts/train_biencoder.sh
+++ scripts/train_biencoder.sh
@@ -6,7 +6,6 @@ CUDA_VISIBLE_DEVICES=1 python3 saturn/train_biencoder.py \
         --model_dir $model_dir \
         --data_dir $data_dir\
         --model_type GPT-HUST \
-        --do_train \
         --num_train_epochs 10 \
         --train_batch_size 1024 \
         --max_seq_len_query 64 \
