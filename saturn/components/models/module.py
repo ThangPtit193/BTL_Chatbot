7c393884c770ad060efaa792ceaed63615153c50
Phương Nam <phuongnamDPN2k2@gmail.com>
2023-10-21 07:17:54 +0700

    Update module.py

--- saturn/components/models/module.py
+++ saturn/components/models/module.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def dot_product_scores(compr: torch.Tensor, refer: torch.Tensor) -> torch.Tensor:
+def dot_product_scores(compr, refer):
     r = torch.matmul(compr, torch.transpose(refer, 0, 1))
     return r
 
