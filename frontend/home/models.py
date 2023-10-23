b9f7c89db2d98ed33ba6a2c71d5367e9b82ad734
Phương Nam <phuongnamDPN2k2@gmail.com>
2023-10-23 11:53:46 +0700

    Update models.py

--- frontend/home/models.py
+++ frontend/home/models.py
@@ -1,49 +1,11 @@
-import re
-import json
 import torch
-vowel = [
-    ["a", "à", "á", "ả", "ã", "ạ", "a"],
-    ["ă", "ằ", "ắ", "ẳ", "ẵ", "ặ", "aw"],
-    ["â", "ầ", "ấ", "ẩ", "ẫ", "ậ", "aa"],
-    ["e", "è", "é", "ẻ", "ẽ", "ẹ", "e"],
-    ["ê", "ề", "ế", "ể", "ễ", "ệ", "ee"],
-    ["i", "ì", "í", "ỉ", "ĩ", "ị", "i"],
-    ["o", "ò", "ó", "ỏ", "õ", "ọ", "o"],
-    ["ô", "ồ", "ố", "ổ", "ỗ", "ộ", "oo"],
-    ["ơ", "ờ", "ớ", "ở", "ỡ", "ợ", "ow"],
-    ["u", "ù", "ú", "ủ", "ũ", "ụ", "u"],
-    ["ư", "ừ", "ứ", "ử", "ữ", "ự", "uw"],
-    ["y", "ỳ", "ý", "ỷ", "ỹ", "ỵ", "y"],
-]
 
-vowel_to_idx = {}
-for i in range(len(vowel)):
-    for j in range(len(vowel[i]) - 1):
-        vowel_to_idx[vowel[i][j]] = (i, j)
+def get_answer(outputs, tokenizer):
+    answer = ""
+    for output in outputs:
+        id = torch.argmax(output).item()
+        if output[id] == 0:
+            continue
+        answer = answer + " " + tokenizer.get_id(id)
 
-
-def load_jsonl(file_path):
-    data = []
-    with open(file_path, "r", encoding="utf-8") as f:
-        for line in f:
-            data.append(json.loads(line))
-    return data
-
-
-def preprocessing(text):
-    punctuation = "[\".?!#$%&'()*+,:;<=>@[\\]^`{|}~]“”"
-    text = text.translate(str.maketrans(' ', ' ', punctuation))
-    text = text.lower()
-
-    # remove duplicate space
-    text = re.sub(r"\s+", " ", text)
-    return text
-
-
-def load_data(file_path, col_meta):
-    data = load_jsonl(file_path)
-    results = []
-    for sentence in data:
-        results.append(sentence[col_meta])
-
-    return results
+    return answer
